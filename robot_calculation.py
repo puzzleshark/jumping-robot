7a2964171c9460e9de8c3263a35a3a97c234fa95
Samuel Banning <samcbanning@gmail.com>
2021-09-08 18:27:27 -0400

    fixed

--- robot_calculation.py
+++ robot_calculation.py
@@ -23,7 +23,7 @@ GRAVITY_ACCELLERATION = 9.8 * ureg.m/ureg.s**2
 
 
 # area is diameter * pi
-piston_area = BORE_SIZE * math.pi * ureg.mm
+piston_area = (BORE_SIZE/2)**2 * np.pi
 
 force_exerted = ((COMPRESSOR_PRESSURE - ATMOS_PRESSURE)*piston_area).to(ureg.newton)
 print("force exerted at each point of piston stroke:", force_exerted)

233eb831dfdbfab076866667537a0deeecf2db21
Samuel Banning <samcbanning@gmail.com>
2021-09-08 18:22:40 -0400

    improved calculation

--- robot_calculation.py
+++ robot_calculation.py
@@ -1,5 +1,6 @@
 import math
 import pint
+import numpy as np
 
 
 ureg = pint.UnitRegistry()
@@ -9,18 +10,32 @@ BORE_SIZE = 32 * ureg.mm  # diameter of piston head
 STROKE = 200 * ureg.mm
 PISTON_WEIGHT = 910 * ureg.g
 
+
+# mass of chassis
+CHASSIS_WEIGHT = 2 * ureg.kg
+
 # compressor constants
-COMPRESSOR_PRESSURE = 100 * ureg.psi
+COMPRESSOR_PRESSURE = 60 * ureg.psi
 
 # natural constants
 ATMOS_PRESSURE = 1 * ureg.bar
-GRAVITY_ACCELLERATION = 9.8 * ureg.m / ureg.s ** 2
+GRAVITY_ACCELLERATION = 9.8 * ureg.m/ureg.s**2
 
 
 
 piston_area = BORE_SIZE * math.pi * ureg.mm  # area is diameter * pi
 
-force_exerted = (COMPRESSOR_PRESSURE - ATMOS_PRESSURE) * piston_area
-force_exerted = force_exerted.to(ureg.newton)
+force_exerted = ((COMPRESSOR_PRESSURE - ATMOS_PRESSURE)*piston_area).to(ureg.newton)
+print("force exerted at each point of piston stroke:", force_exerted)
 
-print(force_exerted)
+energy_exerted = (force_exerted * STROKE).to(ureg.joules)
+print("work done by piston:", energy_exerted)
+
+# gravity is acting on device when piston is expanding upwards
+energy_plus_gravity = energy_exerted - GRAVITY_ACCELLERATION*(PISTON_WEIGHT + CHASSIS_WEIGHT)*STROKE
+print("pison force minus gravity:", energy_plus_gravity)
+
+exit_velocity_squared = (2 * energy_plus_gravity / (PISTON_WEIGHT + CHASSIS_WEIGHT)).to((ureg.m/ureg.s)**2)
+exit_velocity = np.sqrt(exit_velocity_squared)
+print("exit velocity of robot is:", exit_velocity)
+

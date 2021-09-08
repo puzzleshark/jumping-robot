3f1a39e340dd4295718cf723676f43a35ef1cd00
Samuel Banning <samcbanning@gmail.com>
2021-09-08 17:40:33 -0400

    added newtons

--- robot_calculation.py
+++ robot_calculation.py
@@ -8,4 +8,14 @@ BORE_SIZE = 32 * ureg.mm  # diameter of piston head
 STROKE = 200 * ureg.mm
 PISTON_WEIGHT = 910 * ureg.g
 
-piston_area = BORE_SIZE * math.pi
+
+COMPRESSOR_PRESSURE = 100 * ureg.psi
+ATMOS_PRESSURE = 1 * ureg.bar
+
+piston_area = BORE_SIZE * math.pi * ureg.mm  # area is diameter * pi
+
+force_exerted = (COMPRESSOR_PRESSURE - ATMOS_PRESSURE) * piston_area
+
+force_exerted = force_exerted.to(ureg.newton)
+
+print(force_exerted)
